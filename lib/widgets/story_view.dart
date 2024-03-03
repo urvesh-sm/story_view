3a8bb43e57d8cc737e5178fba4a87660af1b7c53
urvesh-sm <127727757+urvesh-sm@users.noreply.github.com>
2024-03-04 01:52:14 +0530

    Update story_view.dart

--- lib/widgets/story_view.dart
+++ lib/widgets/story_view.dart
@@ -649,7 +649,6 @@ class StoryViewState extends State<StoryView> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
       child: Stack(
         children: <Widget>[
           _currentView,
